b686c330f38bad7cc83aad99fd41aef37ce80099
Francisco Jefferson Ferreira de Lima <2119917@ifrn.local>
2019-07-17 14:32:57 -0300

    adcionando comando para zerar cotas de professores

--- grafica/settings.py
+++ grafica/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'professor',
-    'bootstrap3',
-    'zerar_totais'
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
