809e294006c27ca4e9aa925a38d284377d7329c7
Francisco Jefferson Ferreira de Lima <2119917@ifrn.local>
2019-07-17 13:51:19 -0300

    adcionando comando para zerar cotas de professores

--- professor/management/commands/zerar_totais.py
+++ professor/management/commands/zerar_totais.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
     	zerar_totais()
 
-    def zerar_totais():
-    	professores = Usuario.objects.filter(tipo="professor")
-        professores.update(quantidade_atual=0)
-        professores.save()
+def zerar_totais():
+   	professores = Usuario.objects.filter(tipo="professor")
+    professores.update(quantidade_atual=0)
+    professores.save()
