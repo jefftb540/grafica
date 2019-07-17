c202c2653472eff21c8ea70890d6672ffc4a5747
Francisco Jefferson Ferreira de Lima <2119917@ifrn.local>
2019-07-17 13:23:51 -0300

    adcionando comando para zerar cotas de professores

---
+++ professor/management/commands/zerar_totais.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from models import Usuario
+
+
+class Command(BaseCommand):
+    help = 'Zerar cotas dos professores'
+
+    def handle(self, *args, **kwargs):
+        professores = Usuario.objects.filter(tipo="professor")
+        professores.save()
